3c0df6e1be7b813301b3a8195b6c8b67ef6f04d6
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-10-01 08:54:20 +0200

    type get lib version fun

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { getLibVersion } from "./version";
+import type { GetLibVersion } from "../types/version";
 export const pecherCollabProjectId = "pecherCollabProject";
-export const pecherCollabProjectVersion = getLibVersion();
+export const pecherCollabProjectVersion: GetLibVersion = getLibVersion();
