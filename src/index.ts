36507726554275f555e2b524645cf0d39668095f
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-10-01 10:26:11 +0200

    declare module for version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { getLibVersion } from "./version";
-import type { GetLibVersion } from "./types/version";
 export const pecherCollabProjectId = "pecherCollabProject";
-export const pecherCollabProjectVersion: GetLibVersion = getLibVersion();
+export const pecherCollabProjectVersion: string = getLibVersion();
--- src/types/version.d.ts
+++ src/types/version.d.ts
@@ -1,1 +1,11 @@
 export type GetLibVersion = () => string;
+export type GetLibHash = () => string;
+export type GetLibDescribe = () => string;
+export type IsDirty = () => boolean;
+
+declare module "./version" {
+  export const getLibVersion: GetLibVersion;
+  export const getLibHash: GetLibHash;
+  export const getLibDescribe: GetLibDescribe;
+  export const isDirty: IsDirty;
+}
