0727e59691228f9f511fdb372ab82080aee09702
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-10-01 09:19:58 +0200

    move types folder

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { getLibVersion } from "./version";
-import type { GetLibVersion } from "../types/version";
+import type { GetLibVersion } from "./types/version";
 export const pecherCollabProjectId = "pecherCollabProject";
 export const pecherCollabProjectVersion: GetLibVersion = getLibVersion();
