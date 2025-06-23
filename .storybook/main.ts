f875eb96cb2ac452314f7637642a7a131a0e1214
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2025-06-23 09:53:06 +0200

    Revert "migrate nx and storybook to latest"
    This reverts commit 084b0de550d1a5951f8a4bdf9b3f98869c3d9406.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,9 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
-    "@storybook/addon-docs"
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
   ],
 
   framework: {
